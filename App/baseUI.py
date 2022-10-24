2dfe65e6b3095536a738e92e173351a990d39142
tmc42 <tmclean1212@gmail.com>
2022-10-24 15:42:58 -0500

    Initial Commit

---
+++ App/baseUI.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class Main(App):
+    def build(self):
+        return Label(text='Hello World')
+
+
+Main().run()
