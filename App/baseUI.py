4ab7311a59f367d1b13dbd8eb9726157be9cc6b8
tmc42 <tmclean1212@gmail.com>
2022-10-27 14:52:47 -0500

    Added pdf to jpg conversion

--- App/baseUI.py
+++ App/baseUI.py
@@ -5,7 +5,9 @@ from kivymd.app import MDApp
 from kivy.core.window import Window
 from plyer import camera
 
-kv = """
+import os
+
+Builder.load_string("""
 Screen:
     MDBoxLayout:
         orientation: 'vertical'
@@ -56,8 +58,25 @@ Screen:
                     IconLeftWidget:
                         icon: 'information'
                             
-        Widget:
-"""
+       <MyWidget>:
+    id: my_widget
+    Button
+        text: "open"
+        on_release: my_widget.open(filechooser.path, filechooser.selection)
+    FileChooserListView:
+        id: filechooser
+        on_selection: my_widget.selected(filechooser.selection)
+""")
+
+class MyWidget(BoxLayout):
+    def open(self, path, filename):
+        with open(os.path.join(path, filename[0])) as f:
+            print(f.read())
+
+    def selected(self, filename):
+        print("selected: %s" % filename[0])
+
+
 
 class MyApp(MDApp):
 
