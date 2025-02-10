c419e83eeb1d34296eafbf1e03e09f1825063224
tmc42 <tmclean1212@gmail.com>
2025-02-10 16:06:59 -0600

    Complete refactor

--- App/convertpdf.py
+++ App/convertpdf.py
@@ -3,7 +3,7 @@
 from pdf2image import convert_from_path as PDFConv
 import sys
 
-def ConvertPDF(pdf_path, poppler_path = "D:/poppler-22.04.0/Library/bin", saveas_seed = "PDF2IMG"):
+def ConvertPDF(pdf_path, poppler_path = "poppler-22.04.0/Library/bin", saveas_seed = "PDF2IMG"):
     converted_imgs = PDFConv(pdf_path, poppler_path=poppler_path)
     
     for i in range(len(converted_imgs)):
