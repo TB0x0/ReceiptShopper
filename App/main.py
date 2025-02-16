de27c71712532385c371aa97fdace225e927adcd
tmc42 <tmclean1212@gmail.com>
2025-02-15 18:21:39 -0600

    pytesseract implementation

--- App/main.py
+++ App/main.py
@@ -1,6 +1,14 @@
 # TB0x0 2024
 # Receipt Shopper Main
 
-import pytesseract
+import pytesseract as pt
+import preprocess
 
+TEST_IMAGE = 'C:/proj/ReceiptShopper/Tests/testR1.jpg'
 # OCR
+
+def ocrCore(img):
+    receipt, edges, thresh = preprocess.ImgPP(img)
+    return pt.image_to_string(img.fromarray(thresh))
+
+print(ocrCore(TEST_IMAGE))
